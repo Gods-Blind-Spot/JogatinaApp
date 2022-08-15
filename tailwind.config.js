5d7cb3c223638f53270dd64c753cd963442755de
Karonpa <76413505+ViniciusIth@users.noreply.github.com>
2022-08-15 15:04:06 -0300

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,46 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,ts}'],
+  theme: {
+    extend: {},
+    colors: {
+      primary: {
+        default: 'var(--ion-color-primary)',
+        tint: 'var(--ion-color-primary-tint)',
+        shade: 'var(--ion-color-primary-shade)'
+      },
+      secondary: {
+        default: 'var(--ion-color-secondary)',
+        tint: 'var(--ion-color-secondary-tint)',
+        shade: 'var(--ion-color-secondary-shade)'
+
+      },
+      tertiary: {
+        default: 'var(--ion-color-tertiary)',
+        tint: 'var(--ion-color-tertiary-tint)',
+        shade: 'var(--ion-color-tertiary-shade)'
+      },
+      dark: {
+        default: 'var(--ion-color-dark)',
+        tint: 'var(--ion-color-dark-tint)',
+        shade: 'var(--ion-color-dark-shade)'
+      },
+      light: {
+        default: 'var(--ion-color-light)',
+        tint: 'var(--ion-color-light-tint)',
+        shade: 'var(--ion-color-light-shade)'
+      },
+      danger: {
+        default: 'var(--ion-color-danger)',
+        tint: 'var(--ion-color-danger-tint)',
+        shade: 'var(--ion-color-danger-shade)'
+      },
+      warning: {
+        default: 'var(--ion-color-warning)',
+        tint: 'var(--ion-color-warning-tint)',
+        shade: 'var(--ion-color-warning-shade)'
+      },
+    }
+  },
+  plugins: [],
+};
